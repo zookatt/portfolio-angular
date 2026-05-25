667c3baeb5d2e771d9f5184d60141062ff0caa9f
zookat <zotova.ea@gmail.com>
2026-05-25 12:37:29 +0200

    feat: add main components for portfolio including navbar, hero, about, projects, skills, and contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
+import { Hero } from './features/hero/hero';
+import { About } from './features/about/about';
+import { Projects } from './features/projects/projects';
+import { Skills } from './features/skills/skills';
+import { Contact } from './features/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Hero, About, Projects, Skills, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/features/about/about.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About {}
